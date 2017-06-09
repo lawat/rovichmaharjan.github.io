5a1d0735d42a0cf38f7c3623aacc86ee9096bee7
Rovich Maharjan <rovichmaharjan@gmail.com>
2017-06-10 00:27:37 +0545

    set up dev server and add background + name

--- dev/gulpFile.js
+++ dev/gulpFile.js
@@ -1,1 +1,31 @@
+'use strict';
+
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src('./styles/scss/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./styles/'))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('dev', ['sass'], function() {
+
+    browserSync.init({
+        server: "./",
+        port: 8000
+    });
+
+    gulp.watch("styles/scss/**/*.scss", ['sass']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+    gulp.watch("scripts/**/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['dev']);
